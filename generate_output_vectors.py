ad09c95bc80b4bb48d2bf0332b6381f50688a279
twixupmysleeve <pratham.mehta001@gmail.com>
2021-07-03 22:30:15 +0530

    modifying code to debug

--- generate_output_vectors.py
+++ generate_output_vectors.py
@@ -18,7 +18,7 @@ with open('data/labels.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for r in csv_reader:
-        row=r[0]
+        row = r[0].split()
         rows.append((row[0], row[1], row[2]))
         line_count += 1
     print(f'Processed {line_count} lines.')
