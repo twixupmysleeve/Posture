5250a1400cce6a4c4eb52ae336f4d56e3607990b
twixupmysleeve <pratham.mehta001@gmail.com>
2021-07-03 18:16:40 +0530

    ML model using PyTorch

---
+++ network.py
@@ -0,0 +1,31 @@
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import Adam
+
+
+class Model(nn.Module):
+    def __init__(self, input_dims=5, n_actions=5, fc1_dims=8, fc2_dims=8, lr=0.003):
+        super(Model, self).__init__()
+
+        self.input =  nn.Linear(input_dims, fc1_dims)
+        self.fc1 = nn.Linear(fc1_dims, fc2_dims)
+        self.output = nn.Linear(fc2_dims, n_actions)
+
+        self.optim = Adam(self.parameters(), lr=lr)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+
+        self.loss = nn.MSELoss()
+
+        self.to(self.device)
+
+    def forward(self, x):
+        x = self.input(x)
+        x = F.relu(x)
+
+        x = self.fc1(x)
+        x = F.relu(x)
+
+        x = self.output(x)
+
+        return x
