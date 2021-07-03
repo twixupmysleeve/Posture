57e066e7f0024238a01a88a4f747a2ba86434f8f
twixupmysleeve <pratham.mehta001@gmail.com>
2021-07-03 11:17:38 +0530

    created test file for mp

---
+++ mediapipe_test.py
@@ -0,0 +1,52 @@
+import cv2
+import mediapipe
+
+import cv2
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+# For webcam input:
+cap = cv2.VideoCapture('knee_squat.mp4')
+
+with mp_pose.Pose(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.7) as pose:
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            # continue
+            break
+
+        # Flip the image horizontally for a later selfie-view display, and convert
+        # the BGR image to RGB.
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        results = pose.process(image)
+
+        # Draw the pose annotation on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        image_hight, image_width, _ = image.shape
+
+        print(
+            f'Nose coordinates: ('
+            f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP].x * image_width}, '
+            f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP].y * image_hight})'
+        )
+
+        mp_drawing.draw_landmarks(
+            image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        cv2.imshow('MediaPipe Pose', image)
+
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+
+cap.release()
+cv2.destroyAllWindows()
